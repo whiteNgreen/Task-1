2aa09ac285e039b3a39dfb80bc253ac21d565b55
whiteNgreen <33910273+whiteNgreen@users.noreply.github.com>
2020-09-01 14:04:45 +0200

    Add project files.

---
+++ Task 1/Task 1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int age;
+
+int main()
+{
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    if (age <= 19) {
+        std::cout << "You are a teenager";
+    }
+    else {
+        std::cout << "You are an adult";
+    }
+}
+
