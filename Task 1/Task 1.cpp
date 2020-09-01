b309a7e7cacaa980a85e43799d9a0de9fc006a37
whiteNgreen <33910273+whiteNgreen@users.noreply.github.com>
2020-09-01 15:36:13 +0200

    d

--- Task 1/Task 1.cpp
+++ Task 1/Task 1.cpp
@@ -3,16 +3,26 @@
 #include <string>
 
 int age;
+std::string name = "";
 
 int main()
 {
     std::cout << "Enter your age: ";
     std::cin >> age;
-    if (age <= 19) {
+    std::cout << "What is your full name? ";
+    std::cin.ignore();//Sørger for at den venter på neste linje, istedenfor å bare hoppe videre 
+    std::getline(std::cin, name);
+    
+    /*if (age <= 19) {
         std::cout << "You are a teenager";
     }
     else {
-        std::cout << "You are an adult\n" + age;
-    }
+        std::cout << "You are an adult " << age << '\n';
+    }*/
+
+    std::cout << "Your name is " << name << " and you are " << age << " years old" <<'\n';
+    system("pause");
+    return 0;
 }
+
 
